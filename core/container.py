5b986311f00c081aa589973b93a660ad7989650a
EtafCisky <nfgbvm@gmail.com>
2026-03-29 18:07:22 +0800

    Add files via upload

--- core/container.py
+++ core/container.py
@@ -260,7 +260,8 @@ class Container:
             self.player_repository(),
             self.storage_ring_repository(),
             self.config_manager(),
-            self.bounty_repository()  # 添加悬赏仓储
+            self.bounty_repository(),  # 添加悬赏仓储
+            self.json_storage()  # 添加JSON存储
         )
     
     def rift_service(self):
